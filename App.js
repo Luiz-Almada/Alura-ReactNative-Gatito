f964f5b5427b00189fd486ff489921909b3d7093
Luiz Almada <luizalmada@yahoo.com>
2024-02-22 01:39:04 -0300

    Implementa o Reactotron

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "intl";
 import "intl/locale-data/jsonp/pt-BR";
+import Reactotron from 'reactotron-react-native';
 
 import Rotas from "./src/Rotas";
 import TelaPadrao from "./src/componentes/TelaPadrao/index";
 
+Reactotron.configure().useReactNative().connect();
+
 export default function App() {
+  console.tron = Reactotron;
+  console.tron.log({
+    curso: "Alura"
+  });
   return <Rotas />
 }
